2a64451b06eefab309132a201d995d3861e6943f
AdamBachorz <adam.bachorz1702@gmail.com>
2021-04-01 19:10:37 +0200

    Add "Model" class library

---
+++ VirtualWallet.Model/Domain/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace VirtualWallet.Model.Domain
+{
+    public abstract class Entity
+    {
+        public virtual int Id { get; set; }
+    }
+}
