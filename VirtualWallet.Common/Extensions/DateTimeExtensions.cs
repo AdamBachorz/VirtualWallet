479649c45ecfa004d74b1c22a9ea8f7143dfc68a
AdamBachorz <adam.bachorz1702@gmail.com>
2021-04-11 09:09:47 +0200

    Add DataTimeExtensions

---
+++ VirtualWallet.Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualWallet.Common.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string GetMonthName(this DateTime dateTime)
+        {
+            return "";
+        }
+    }
+}
---
+++ VirtualWallet.Tests/Common/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VirtualWallet.Common.Extensions;
+
+namespace VirtualWallet.Tests.Common.Extensions
+{
+    [TestFixture]
+    public class DateTimeExtensionsTests
+    {
+        [TestCase(1, ExpectedResult = "Styczeń")]
+        [TestCase(2, ExpectedResult = "Luty")]
+        [TestCase(3, ExpectedResult = "Marzec")]
+        [TestCase(4, ExpectedResult = "Kwiecień")]
+        [TestCase(5, ExpectedResult = "Maj")]
+        [TestCase(6, ExpectedResult = "Czerwiec")]
+        [TestCase(7, ExpectedResult = "Lipiec")]
+        [TestCase(8, ExpectedResult = "Sierpień")]
+        [TestCase(9, ExpectedResult = "Wrzesień")]
+        [TestCase(10, ExpectedResult = "Październik")]
+        [TestCase(11, ExpectedResult = "Listopad")]
+        [TestCase(12, ExpectedResult = "Grudzień")]
+        public string ShouldGetMonthName(int month)
+        {
+            return new DateTime(2021, month, 1).GetMonthName();
+        }
+    }
+}
