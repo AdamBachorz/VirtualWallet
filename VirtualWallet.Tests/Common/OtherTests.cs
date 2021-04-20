c95ed4fc64a250d76247a2c4a699144ee73f7213
AdamBachorz <adam.bachorz1702@gmail.com>
2021-04-20 13:00:18 +0200

    Create OtherTests.cs

---
+++ VirtualWallet.Tests/Common/OtherTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VirtualWallet.Common.Extensions;
+
+namespace VirtualWallet.Tests.Common
+{
+    [TestFixture]
+    public class OtherTests
+    {
+        [Test]
+        [Explicit]
+        public void EncrtypTest()
+        {
+            var x = "".Encrypt(); 
+        }
+    }
+}
