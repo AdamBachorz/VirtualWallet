2343764d8712fdf0b17628f0804344532df86c00
AdamBachorz <adam.bachorz1702@gmail.com>
2021-05-05 11:31:28 +0200

    Add QuickLog

--- VirtualWallet.WebApp/Controllers/HomeController.cs
+++ VirtualWallet.WebApp/Controllers/HomeController.cs
@@ -6,16 +6,30 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using VirtualWallet.WebApp.Models;
+using VirtualWallet.Model.Domain;
+using VirtualWallet.ApiConsumer.Interfaces;
+using VirtualWallet.Common.Extensions;
+using VirtualWallet.DAL.Services.Interfaces;
+using VirtualWallet.Model.Classes;
+using System.Net;
 
 namespace VirtualWallet.WebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUserContainer _userContainer;
+        private readonly IUserApiConsumer _userApiConsumer;
+        private readonly ISpendingGroupApiConsumer _spendingGroupApiConsumer;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, 
+            IUserContainer userContainer, IUserApiConsumer userApiConsumer, ISpendingGroupApiConsumer spendingGroupApiConsumer)
         {
             _logger = logger;
+            _userContainer = userContainer;
+            _userApiConsumer = userApiConsumer;
+            _spendingGroupApiConsumer = spendingGroupApiConsumer;
         }
 
         public IActionResult Index()
@@ -32,6 +46,17 @@ namespace VirtualWallet.WebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult QuickLog()
+        {
+            var credential = new NetworkCredential("admin", "admintest");
+            _userApiConsumer.SetAuthorization(credential);
+            _spendingGroupApiConsumer.SetAuthorization(credential);
+            var user = _userApiConsumer.GetByToken(credential.BuildBase64Token());
+            _userContainer.SignIn(user, "admintest");
+            _userContainer.SetSpendingGroup(_spendingGroupApiConsumer.GetForUser(user.Id).First());
+            return RedirectToAction("Current", "MonthlySpending");
         }
     }
 }
